7d8ad61495cff1788b8afcb7149970e42914f1e9
Abhay3008 <wahh0506@gmail.com>
2020-05-25 12:18:00 +0530

    fi

--- prog.py
+++ prog.py
@@ -1,104 +1,56 @@
 from keras.datasets import mnist
-print("h")
+import cv2
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers.convolutional import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+import os
+from keras.optimizers import RMSprop
+from keras.utils.np_utils import to_categorical
+
 dataset = mnist.load_data('mymnist.db')
-
-len(dataset)
-
-dataset
 
 train , test = dataset
 
-type(train)
-
 X_train , y_train = train
 
-X_train.shape
-
 X_test , y_test = test
-
-X_test.shape
-
-img1 = X_train[7]
-
-img1.shape
-
-import cv2
-
-img1_label = y_train[7]
-
-img1_label
-
-img1.shape
-
-#import matplotlib.pyplot as plt
-
-#plt.imshow(img1 , cmap='gray')
-
-#img1.shape
-
-img1_1d = img1.reshape(28*28)
-
-img1_1d.shape
-
-X_train.shape
 
 X_train_1d = X_train.reshape(-1 , 28*28)
 X_test_1d = X_test.reshape(-1 , 28*28)
 
-X_train_1d.shape
-
 X_train = X_train_1d.astype('float32')
 X_test = X_test_1d.astype('float32')
 
-X_train.shape
-
-
 X_train = X_train.reshape(-1,28,28,1)
+X_test = X_test.reshape(-1,28,28,1)
 
 from keras.utils.np_utils import to_categorical
 
 y_train_cat = to_categorical(y_train)
-
-y_train_cat
-
-#y_train_cat[7]
-
-from keras.models import Sequential
-
-from keras.layers import Dense
-
-from keras.layers.convolutional import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-
+y_test_cat = to_categorical(y_test)
 
 model = Sequential()
+if(os.environ['trial']>='2'):
+ model.add(Conv2D(32,(5,5),input_shape = (28,28,1),activation = 'relu'))
 
-model.add(Conv2D(32,(3,3),input_shape = (28,28,1),activation = 'relu'))
-
-model.summary()
-
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(6,6)))
 
 model.add(Flatten())
-
-model.add(Dense(units=256, activation='relu'))
-
-model.summary()
+if(os.environ['trial']>='3'):
+ model.add(Dense(units=64, activation='relu'))
 
 model.add(Dense(units=10, activation='softmax'))
 
-model.summary()
-
-from keras.optimizers import RMSprop
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', 
              metrics=['accuracy']
              )
 
-model.layers.pop()
-
-model.summary()
-
-h = model.fit(X_train, y_train_cat,batch_size=512)
+h = model.fit(X_train, y_train_cat,batch_size=512,epochs=1)
+test_loss, test_acc = model.evaluate(X_test, y_test_cat)
+print(test_acc)
+with open('Output.txt', 'x') as f:
+  print(test_acc, file=f)
 model.save("digit.h5")
